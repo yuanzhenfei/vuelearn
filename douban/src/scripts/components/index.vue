02420f239431abcbfe3e3a56a8a9cabf54eaed98
YuanZhenfei <yzfCode@163.com>
2018-11-12 07:29:11 +0800

    第一次完善

---
+++ douban/src/scripts/components/index.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <div class="m-index">
+    <header>
+      header
+    </header>
+    <section>
+
+    </section>
+    <nav>
+
+    </nav>
+  </div>
+</template>
+<script>
+  export default {
+
+  }
+</script>
+<style lang="scss">
+  html,body{
+    padding: 0;
+    margin: 0;
+    height: 100%;
+    width: 100%;
+  }
+  #app{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+  .m-index{
+    height: 100%;
+    width: 100%;
+    background: red;
+  }
+</style>
